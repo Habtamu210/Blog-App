28ea13e30293a1af0c3c53c5db9878e0808c45b2
Habtamu210 <beleyaha@gmail.com>
2023-05-23 03:48:12 -0700

    Improve query optimization performance on posts

--- app/controllers/comments_contoller.rb
+++ app/controllers/comments_contoller.rb
@@ -1,36 +1,33 @@
 class CommentsController < ApplicationController
   def new
     @comment = Comment.new
-    @post = Post.find(params[:post_id])
+    # @current_user = current_user
+    # @comment.author = @current_user
+    # @user = User.find(params[:user_id])
+    @post = Post.find(params[:id])
   end
 
   def create
-    post = Post.find(params[:post_id])
-    comment = Comment.create(author: current_user, post:, **comment_params)
+    @comment = Comment.new(comments_params)
+    @current_user = current_user
+    @post = Post.find(params[:id])
+    @comment.author = @current_user
+    @comment.post = @post
 
-    if comment.save
-      flash[:success] = 'Comment was successfully saved !'
+    if @comment.save
+      flash.now[:notice] = 'Commented successfully!'
+      redirect_to post_show_path(@current_user)
     else
-      flash.now[:error] = 'Error: Comment could not be saved'
+      flash.now[:error] = 'Commenting failed!!'
+      render 'new'
     end
-    redirect_to user_post_url(current_user.id, post.id)
   end
 
-  def destroy
-    user = User.find(params[:user_id])
-    post = user.posts.find(params[:post_id])
-    comment = Comment.find(params[:id])
-    if comment.destroy
-      flash[:success] = 'Comment was successfully deleted.'
-    else
-      flash[:error] = 'Error: Comment could not be deleted'
-    end
-    redirect_to user_post_path(user, post)
-  end
+  def under_construction; end
 
   private
 
-  def comment_params
-    params.require(:comment).permit(:text)
+  def comments_params
+    params.require(:comment).permit(:text, :author_id, :post_id)
   end
 end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,36 +1,24 @@
-class CommentsController < ApplicationController
+class LikesController < ApplicationController
   def new
-    @comment = Comment.new
-    @post = Post.find(params[:post_id])
+    @like = Like.new
+    @current_user = current_user
   end
 
   def create
-    post = Post.find(params[:post_id])
-    comment = Comment.create(author: current_user, post:, **comment_params)
+    @like = Like.new(like_params)
+    @like.author = current_user
 
-    if comment.save
-      flash[:success] = 'Comment was successfully saved !'
+    if @like.save
+      flash.now[:success] = 'You just liked it'
+      redirect_to post_show_path(@current_user)
     else
-      flash.now[:error] = 'Error: Comment could not be saved'
+      flash.now[:alert] = 'Like creation failed'
     end
-    redirect_to user_post_url(current_user.id, post.id)
-  end
-
-  def destroy
-    user = User.find(params[:user_id])
-    post = user.posts.find(params[:post_id])
-    comment = Comment.find(params[:id])
-    if comment.destroy
-      flash[:success] = 'Comment was successfully deleted.'
-    else
-      flash[:error] = 'Error: Comment could not be deleted'
-    end
-    redirect_to user_post_path(user, post)
   end
 
   private
 
-  def comment_params
-    params.require(:comment).permit(:text)
+  def like_params
+    params.require(:like).permit(:post_id)
   end
 end
