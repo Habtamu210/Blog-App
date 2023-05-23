7f623adbe4ed04398f0ef2a2a599f9c2b6dc12b3
Habtamu210 <beleyaha@gmail.com>
2023-05-23 05:10:04 -0700

    Made a migration on the database

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,18 @@
 Rails.application.routes.draw do
-  #devise root
-  devise_for :users
-  devise_scope :user do
-    root to: "devise/sessions#new"
-  end
 
-   # API routes
-   namespace :api do
-    namespace :v1 do
-      resources :users do 
-        resources :posts do 
-          resources :comments
-          resources :likes
-        end
-      end
-    end
-  end
+  resources :users, only: [:index, :show]
 
-  resources :users, only: [:index, :show] do 
-    resources :posts, only: [:index, :show, :new, :create, :destroy] do 
-      resources :comments, only: [:new, :create, :destroy]
-      resources :likes, only: [:create]
-    end
-  end
+  resources :posts, only: [:new, :create]
+
+  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
+  get '/users/:user_id/posts/:id', to: 'posts#show', as: 'post_show'
+  post '/users/:user_id/posts/:id/comments', to: 'comments#create', as: 'comment_create'
+  get '/users/:user_id/posts/:id/comments/new', to: 'comments#new', as: 'comment_new'
+  post '/users/:user_id/posts/:id/likes', to: 'likes#create', as: 'like_create'
+  get '/users/:user_id/posts/:id/likes/new', to: 'likes#new', as: 'like_new'
+
+  get '/uncompleted', to: "comments#under_construction"
+
+
+
 end
