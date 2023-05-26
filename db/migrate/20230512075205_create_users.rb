bec415553759a35fc19fe7fd16ba458b40ff1530
Habtamu210 <beleyaha@gmail.com>
2023-05-26 03:31:04 -0700

    Add database migration on the devise features

--- db/migrate/20230512075205_create_users.rb
+++ db/migrate/20230512075205_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.text :name
-      t.string :photo
+      t.string :photo, default: 'https://coffee.alexflipnote.dev/random'
       t.text :bio
       t.integer :posts_count, default: 0
 
