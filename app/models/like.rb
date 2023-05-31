0dd1d389ec24cd8012394b8c9f1a299c75218ea2
Habtamu210 <beleyaha@gmail.com>
2023-05-31 04:31:40 -0700

    Commit changes likes in the model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,12 @@
 class Like < ApplicationRecord
-  belongs_to :author, foreign_key: :author_id, class_name: 'User'
-  belongs_to :post, foreign_key: :author_id, counter_cache: :likes_counter, class_name: 'Post'
+  after_save :update_likes_counter
 
-  def update_like_counter
-    post.increment!(:likes_counter)
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post
+
+  private
+
+  def update_likes_counter
+    post.update(likes_counter: post.likes.all.length)
   end
 end
